df42d0597a787a1a26e559d9c453454520e83395
Herbert Richard <hebert.richard0800@gmail.com>
2025-01-18 19:58:32 -0300

    Audio testing succefully complete

--- audio-testing.py
+++ audio-testing.py
@@ -1,12 +1,13 @@
 import sounddevice as sd
+from scipy.io.wavfile import write
 
-#git testing
 
 sd.default.samplerate = 44100
 sd.default.channels = 2
 
+print("Recording...")
 myaudio = sd.rec(int(3 * sd.default.samplerate))
 
-sd.wait()
 
-print(myaudio)
+sd.wait()
+write("sounddevice-audio.wav", 44100, myaudio)
