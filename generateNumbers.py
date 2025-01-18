a1534e28883669be326303974056d757b95863a8
Herbert Richard <hebert.richard0800@gmail.com>
2025-01-18 20:53:57 -0300

    Add generateNumbers.py

---
+++ generateNumbers.py
@@ -0,0 +1,15 @@
+from random import randrange
+
+def generateNumbers():
+    randomNumbers = [0, 0, 0, 0, 0]
+    x = 0
+    while(x != 5):
+        randomNumbers[x] = randrange(0, 1000)
+        randomNumbers[x] = str(randomNumbers[x])
+        x += 1
+
+
+    stringRandomNumbers = " ".join(randomNumbers)
+    return stringRandomNumbers
+
+print(generateNumbers())
--- whisper-testing.py
+++ whisper-testing.py
@@ -1,12 +1,19 @@
 import whisper
 import sounddevice as sd
 from scipy.io.wavfile import write 
+from random import randrange
+
+
+
 
 #SOUNDDEVICE config
 sd.default.samplerate = 44100
 sd.default.channels = 2
 audioDuration = 8
 userAudioFile = "userAudio.wav"
+
+
+#Random number generator
 
 #WHISPER config
 model = whisper.load_model('base.en')
