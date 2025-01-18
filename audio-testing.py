283d42bd13b34782e454dced9055b0b00a2079f0
Herbert Richard <hebert.richard0800@gmail.com>
2025-01-18 20:12:43 -0300

    App MVP Done

--- audio-testing.py
+++ audio-testing.py
@@ -8,6 +8,5 @@ sd.default.channels = 2
 print("Recording...")
 myaudio = sd.rec(int(3 * sd.default.samplerate))
 
-
 sd.wait()
 write("sounddevice-audio.wav", 44100, myaudio)
--- whisper-testing.py
+++ whisper-testing.py
@@ -1,15 +1,29 @@
 import whisper
+import sounddevice as sd
+from scipy.io.wavfile import write 
 
+#SOUNDDEVICE config
+sd.default.samplerate = 44100
+sd.default.channels = 2
+audioDuration = 8
+userAudioFile = "userAudio.wav"
 
+#WHISPER config
 model = whisper.load_model('base.en')
 
-#RECORD THE AUDIO
+#Record and Save Audio
+print("Recording...")
+userAudio = sd.rec(int(audioDuration * sd.default.samplerate))
+sd.wait()
 
-result = model.transcribe("number-test.mp3")
+write(userAudioFile, 44100, userAudio)
 
+
+result = model.transcribe(userAudioFile)
+
+#Compare result
 if(result["text"].strip() == "2056 12 18"):
     print("You speaked correctly!")
 else:
-    print("You pronounced incorrectly, correct answer: " + result["text"])
+    print("You pronounced incorrectly, your answer: " + result["text"])
 
-    
