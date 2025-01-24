d44b70307fd4ea85cbbce39949024f28b9eceb0a
Herbert Richard <hebert.richard0800@gmail.com>
2025-01-23 22:15:32 -0300

    First test with Python eel

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-import whisper
-from generateNumbers import generateNumbers
-from recordAudio import recordAudio
-from compareNumbers import compareNumbers
-#WHISPER config
-model = whisper.load_model('base.en')
+import eel
 
-#Record and Save Audio
-def recordProcess():
-  randomNumbers = generateNumbers()
-  print(randomNumbers)
-  print("Recording...")
-  userAudioFile = recordAudio(10)
-  #Use Whipser to transcribe Audio
-  transcribedAudio = model.transcribe(userAudioFile)
-  compareNumbers(transcribedAudio['text'], randomNumbers)
-  
-  continueQuestion = input("Quer tentat novamente? (S/N)")
-  
-  if(continueQuestion.upper() == 'S'):
-    recordProcess()
+#EEL config
+eel.init('static_web_folder')
 
+@eel.expose
+def helloFunc():
+    print('hello world')
 
-#TESTE DE COMMIT!
-recordProcess()
-
-
-
-
-
-
+eel.start('main.html')
