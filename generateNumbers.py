51188309619d21dd368b60cef6772b3962dc67d4
Herbert Richard <hebert.richard0800@gmail.com>
2025-02-06 23:08:00 -0300

    increase number size

--- generateNumbers.py
+++ generateNumbers.py
@@ -1,9 +1,9 @@
 from random import randrange
 
 def generateNumbers():
-    randomNumbers = [0, 0]
+    randomNumbers = [0, 0, 0]
     x = 0
-    while(x != 2):
+    while(x != 3):
         randomNumbers[x] = randrange(0, 1000)
         randomNumbers[x] = str(randomNumbers[x])
         x += 1
